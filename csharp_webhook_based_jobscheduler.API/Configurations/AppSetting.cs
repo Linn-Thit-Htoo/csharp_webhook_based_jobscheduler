eabbb9e639efd89cd275cf955a7fd0d0d32286b2
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-23 23:45:25 +0630

    -

--- csharp_webhook_based_jobscheduler.API/Configurations/AppSetting.cs
+++ csharp_webhook_based_jobscheduler.API/Configurations/AppSetting.cs
@@ -1,24 +1,23 @@
-﻿namespace csharp_webhook_based_jobscheduler.API.Configurations
+﻿namespace csharp_webhook_based_jobscheduler.API.Configurations;
+
+public class AppSetting
 {
-    public class AppSetting
-    {
-        public Connectionstrings ConnectionStrings { get; set; }
-        public Logging Logging { get; set; }
-    }
+    public Connectionstrings ConnectionStrings { get; set; }
+    public Logging Logging { get; set; }
+}
 
-    public class Connectionstrings
-    {
-        public string DbConnection { get; set; }
-    }
+public class Connectionstrings
+{
+    public string DbConnection { get; set; }
+}
 
-    public class Logging
-    {
-        public Loglevel LogLevel { get; set; }
-    }
+public class Logging
+{
+    public Loglevel LogLevel { get; set; }
+}
 
-    public class Loglevel
-    {
-        public string Default { get; set; }
-        public string MicrosoftAspNetCore { get; set; }
-    }
+public class Loglevel
+{
+    public string Default { get; set; }
+    public string MicrosoftAspNetCore { get; set; }
 }
