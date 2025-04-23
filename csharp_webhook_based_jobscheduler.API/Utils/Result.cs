d4133211d7c6c75e3bb9f35440a8370a6c848e69
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-24 00:14:32 +0630

    -

--- csharp_webhook_based_jobscheduler.API/Utils/Result.cs
+++ csharp_webhook_based_jobscheduler.API/Utils/Result.cs
@@ -1,71 +1,70 @@
 ﻿using System.Net;
 
-namespace csharp_webhook_based_jobscheduler.API.Utils
+namespace csharp_webhook_based_jobscheduler.API.Utils;
+
+public class Result<T>
 {
-    public class Result<T>
+    public HttpStatusCode StatusCode { get; set; }
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; } = null!;
+    public T? Data { get; set; }
+
+    public static Result<T> Success(string message = "Success")
     {
-        public HttpStatusCode StatusCode { get; set; }
-        public bool IsSuccess { get; set; }
-        public string Message { get; set; } = null!;
-        public T? Data { get; set; }
-
-        public static Result<T> Success(string message = "Success")
+        return new Result<T>
         {
-            return new Result<T>
-            {
-                Message = message,
-                IsSuccess = true,
-                StatusCode = HttpStatusCode.OK,
-            };
-        }
+            Message = message,
+            IsSuccess = true,
+            StatusCode = HttpStatusCode.OK,
+        };
+    }
 
-        public static Result<T> Success(T data, string message = "Success")
+    public static Result<T> Success(T data, string message = "Success")
+    {
+        return new Result<T>
         {
-            return new Result<T>
-            {
-                Data = data,
-                IsSuccess = true,
-                Message = message,
-                StatusCode = HttpStatusCode.OK,
-            };
-        }
+            Data = data,
+            IsSuccess = true,
+            Message = message,
+            StatusCode = HttpStatusCode.OK,
+        };
+    }
 
-        public static Result<T> Fail(
-            string message = "Fail.",
-            HttpStatusCode statusCode = HttpStatusCode.BadRequest
-        )
+    public static Result<T> Fail(
+        string message = "Fail.",
+        HttpStatusCode statusCode = HttpStatusCode.BadRequest
+    )
+    {
+        return new Result<T>
         {
-            return new Result<T>
-            {
-                IsSuccess = false,
-                Message = message,
-                StatusCode = statusCode,
-            };
-        }
+            IsSuccess = false,
+            Message = message,
+            StatusCode = statusCode,
+        };
+    }
 
-        public static Result<T> Fail(Exception ex)
+    public static Result<T> Fail(Exception ex)
+    {
+        return new Result<T>
         {
-            return new Result<T>
-            {
-                IsSuccess = false,
-                Message = ex.ToString(),
-                StatusCode = HttpStatusCode.InternalServerError,
-            };
-        }
+            IsSuccess = false,
+            Message = ex.ToString(),
+            StatusCode = HttpStatusCode.InternalServerError,
+        };
+    }
 
-        public static Result<T> NotFound(string message = "No data found.")
-        {
-            return Result<T>.Fail(message, HttpStatusCode.NotFound);
-        }
+    public static Result<T> NotFound(string message = "No data found.")
+    {
+        return Result<T>.Fail(message, HttpStatusCode.NotFound);
+    }
 
-        public static Result<T> Duplicate(string message = "Duplicate data.")
+    public static Result<T> Duplicate(string message = "Duplicate data.")
+    {
+        return new Result<T>
         {
-            return new Result<T>
-            {
-                IsSuccess = false,
-                Message = message,
-                StatusCode = HttpStatusCode.Conflict,
-            };
-        }
+            IsSuccess = false,
+            Message = message,
+            StatusCode = HttpStatusCode.Conflict,
+        };
     }
 }
