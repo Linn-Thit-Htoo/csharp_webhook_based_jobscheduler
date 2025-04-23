0fc1d14f09e42cc9b49329ebd8950a4134fc89d3
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-23 23:46:33 +0630

    -

--- csharp_webhook_based_jobscheduler.API/Extensions/Extension.cs
+++ csharp_webhook_based_jobscheduler.API/Extensions/Extension.cs
@@ -4,7 +4,8 @@ namespace csharp_webhook_based_jobscheduler.API.Extensions;
 
 public static class Extension
 {
-    public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj, Formatting.Indented);
+    public static string ToJson(this object obj) =>
+        JsonConvert.SerializeObject(obj, Formatting.Indented);
 
     public static T ToObject<T>(this string json) => JsonConvert.DeserializeObject<T>(json)!;
 }
