80485547022ffef39a323acfdeade5c0980e22fa
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-24 00:14:15 +0630

    -

--- csharp_webhook_based_jobscheduler.API/Services/HttpClientServices/HttpClientService.cs
+++ csharp_webhook_based_jobscheduler.API/Services/HttpClientServices/HttpClientService.cs
@@ -3,58 +3,57 @@ using Hangfire;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
 
-namespace csharp_webhook_based_jobscheduler.API.Services.HttpClientServices
+namespace csharp_webhook_based_jobscheduler.API.Services.HttpClientServices;
+
+public class HttpClientService : IHttpClientService
 {
-    public class HttpClientService : IHttpClientService
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly ILogger<HttpClientService> _logger;
+
+    public HttpClientService(
+        IHttpClientFactory httpClientFactory,
+        ILogger<HttpClientService> logger
+    )
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly ILogger<HttpClientService> _logger;
+        _httpClientFactory = httpClientFactory;
+        _logger = logger;
+    }
 
-        public HttpClientService(
-            IHttpClientFactory httpClientFactory,
-            ILogger<HttpClientService> logger
-        )
+    [AutomaticRetry(Attempts = 3)]
+    public async Task SendAsync(
+        JobSchedulerRequestDto schedulerRequestDto
+    )
+    {
+        try
         {
-            _httpClientFactory = httpClientFactory;
-            _logger = logger;
-        }
-
-        [AutomaticRetry(Attempts = 3)]
-        public async Task SendAsync(
-            JobSchedulerRequestDto schedulerRequestDto
-        )
-        {
-            try
+            HttpClient client = _httpClientFactory.CreateClient();
+            HttpRequestMessage httpRequestMessage = new()
             {
-                HttpClient client = _httpClientFactory.CreateClient();
-                HttpRequestMessage httpRequestMessage = new()
+                Content = new StringContent(
+                    schedulerRequestDto.JsonPayload ?? string.Empty,
+                    Encoding.UTF8,
+                    Application.Json
+                ),
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(schedulerRequestDto.Uri, schedulerRequestDto.Endpoint),
+                Headers =
                 {
-                    Content = new StringContent(
-                        schedulerRequestDto.JsonPayload ?? string.Empty,
-                        Encoding.UTF8,
-                        Application.Json
-                    ),
-                    Method = HttpMethod.Post,
-                    RequestUri = new Uri(schedulerRequestDto.Uri, schedulerRequestDto.Endpoint),
-                    Headers =
-                    {
-                        { "Accept", Application.Json },
-                        { "Accept-Charset", "utf-8" },
-                        { "User-Agent", "WebHookJobScheduler" },
-                    },
-                };
+                    { "Accept", Application.Json },
+                    { "Accept-Charset", "utf-8" },
+                    { "User-Agent", "WebHookJobScheduler" },
+                },
+            };
 
-                using HttpResponseMessage response = await client.SendAsync(
-                    httpRequestMessage,
-                    schedulerRequestDto.Cs
-                );
-                response.EnsureSuccessStatusCode();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error occurred while sending HTTP request.");
-                throw;
-            }
+            using HttpResponseMessage response = await client.SendAsync(
+                httpRequestMessage,
+                schedulerRequestDto.Cs
+            );
+            response.EnsureSuccessStatusCode();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while sending HTTP request.");
+            throw;
         }
     }
 }
