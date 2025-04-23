3ab65905fbe65d729959bc5fea95dd7100b6d85e
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-23 23:45:48 +0630

    -

--- csharp_webhook_based_jobscheduler.API/Constants/ApplicationConstants.cs
+++ csharp_webhook_based_jobscheduler.API/Constants/ApplicationConstants.cs
@@ -4,6 +4,8 @@ namespace csharp_webhook_based_jobscheduler.API.Constants;
 
 public class ApplicationConstants
 {
-    public static TimeZoneInfo GetMyanmarTimeZone() => TimeZoneInfo.FindSystemTimeZoneById("Myanmar Standard Time");
+    public static TimeZoneInfo GetMyanmarTimeZone() =>
+        TimeZoneInfo.FindSystemTimeZoneById("Myanmar Standard Time");
+
     public static TimeZoneInfo GetMyanmarTimeZoneV1() => TZConvert.GetTimeZoneInfo("Asia/Yangon");
 }
