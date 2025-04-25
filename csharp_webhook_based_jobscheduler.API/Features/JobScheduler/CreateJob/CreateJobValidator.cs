ed470159c60b5d74bdb3a5c1919ebbfdcdc4372c
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-26 00:08:53 +0630

    -

--- csharp_webhook_based_jobscheduler.API/Features/JobScheduler/CreateJob/CreateJobValidator.cs
+++ csharp_webhook_based_jobscheduler.API/Features/JobScheduler/CreateJob/CreateJobValidator.cs
@@ -1,21 +1,20 @@
-﻿namespace csharp_webhook_based_jobscheduler.API.Features.JobScheduler.CreateJob
+﻿namespace csharp_webhook_based_jobscheduler.API.Features.JobScheduler.CreateJob;
+
+public class CreateJobValidator : AbstractValidator<CreateJobCommand>
 {
-    public class CreateJobValidator : AbstractValidator<CreateJobCommand>
+    public CreateJobValidator()
     {
-        public CreateJobValidator()
-        {
-            RuleFor(x => x.JobType)
-                .NotNull().NotEmpty()
-                .IsInEnum()
-                .WithMessage("Job Type is invalid.");
+        RuleFor(x => x.JobType)
+            .NotNull().NotEmpty()
+            .IsInEnum()
+            .WithMessage("Job Type is invalid.");
 
-            RuleFor(x => x.Uri)
-                .NotNull().NotEmpty()
-                .WithMessage("Invalid Uri.");
+        RuleFor(x => x.Uri)
+            .NotNull().NotEmpty()
+            .WithMessage("Invalid Uri.");
 
-            RuleFor(x => x.Endpoint)
-                .NotNull().NotEmpty()
-                .WithMessage("Invalid endpoint.");
-        }
+        RuleFor(x => x.Endpoint)
+            .NotNull().NotEmpty()
+            .WithMessage("Invalid endpoint.");
     }
 }
