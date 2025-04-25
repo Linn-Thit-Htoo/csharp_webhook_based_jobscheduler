4393235573b8a618bdd86c0bc4300a9942924a1b
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-26 00:07:20 +0630

    -

--- csharp_webhook_based_jobscheduler.API/Features/JobScheduler/Core/JobSchedulerController.cs
+++ csharp_webhook_based_jobscheduler.API/Features/JobScheduler/Core/JobSchedulerController.cs
@@ -1,21 +1,20 @@
-﻿namespace csharp_webhook_based_jobscheduler.API.Features.JobScheduler.Core
+﻿namespace csharp_webhook_based_jobscheduler.API.Features.JobScheduler.Core;
+
+[Route("api/[controller]")]
+[ApiController]
+public class JobSchedulerController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class JobSchedulerController : BaseController
+    private readonly ISender _sender;
+
+    public JobSchedulerController(ISender sender)
     {
-        private readonly ISender _sender;
+        _sender = sender;
+    }
 
-        public JobSchedulerController(ISender sender)
-        {
-            _sender = sender;
-        }
-
-        [HttpPost("ScheduleJob")]
-        public async Task<IActionResult> ScheduleJob(CreateJobCommand command, CancellationToken cs)
-        {
-            var result = await _sender.Send(command, cs);
-            return Content(result);
-        }
+    [HttpPost("ScheduleJob")]
+    public async Task<IActionResult> ScheduleJob(CreateJobCommand command, CancellationToken cs)
+    {
+        var result = await _sender.Send(command, cs);
+        return Content(result);
     }
 }
