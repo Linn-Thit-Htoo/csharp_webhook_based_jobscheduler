8ea18125bdad09959e95dc310b7b49ffb94fc9ef
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-26 00:07:51 +0630

    -

--- csharp_webhook_based_jobscheduler.API/Features/JobScheduler/CreateJob/CreateJobCommand.cs
+++ csharp_webhook_based_jobscheduler.API/Features/JobScheduler/CreateJob/CreateJobCommand.cs
@@ -1,13 +1,12 @@
-﻿namespace csharp_webhook_based_jobscheduler.API.Features.JobScheduler.CreateJob
+﻿namespace csharp_webhook_based_jobscheduler.API.Features.JobScheduler.CreateJob;
+
+public class CreateJobCommand : IRequest<Result<CreateJobResponse>>
 {
-    public class CreateJobCommand : IRequest<Result<CreateJobResponse>>
-    {
-        public EnumJobType JobType { get; set; }
-        public string Uri { get; set; }
-        public string Endpoint { get; set; }
-        public string? JsonPayload { get; set; }
-        public DateTime? DelayAt { get; set; }
-        public string? CronExpression { get; set; }
-        public string? JobId { get; set; }
-    }
+    public EnumJobType JobType { get; set; }
+    public string Uri { get; set; }
+    public string Endpoint { get; set; }
+    public string? JsonPayload { get; set; }
+    public DateTime? DelayAt { get; set; }
+    public string? CronExpression { get; set; }
+    public string? JobId { get; set; }
 }
