59ce018980f2cdc4dbf73a0c76aee943b02cc331
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-26 00:08:24 +0630

    -

--- csharp_webhook_based_jobscheduler.API/Features/JobScheduler/CreateJob/CreateJobResponse.cs
+++ csharp_webhook_based_jobscheduler.API/Features/JobScheduler/CreateJob/CreateJobResponse.cs
@@ -1,6 +1,5 @@
-﻿namespace csharp_webhook_based_jobscheduler.API.Features.JobScheduler.CreateJob
+﻿namespace csharp_webhook_based_jobscheduler.API.Features.JobScheduler.CreateJob;
+
+public class CreateJobResponse
 {
-    public class CreateJobResponse
-    {
-    }
 }
