52731c589142d78d970d00465d3ed595821816ca
Lin Thit Htoo <lth1212001@gmail.com>
2025-04-26 00:06:59 +0630

    -

--- csharp_webhook_based_jobscheduler.API/Features/Core/BaseController.cs
+++ csharp_webhook_based_jobscheduler.API/Features/Core/BaseController.cs
@@ -1,12 +1,11 @@
-﻿namespace csharp_webhook_based_jobscheduler.API.Features.Core
+﻿namespace csharp_webhook_based_jobscheduler.API.Features.Core;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object obj)
     {
-        protected IActionResult Content(object obj)
-        {
-            return Content(JsonConvert.SerializeObject(obj), "application/json");
-        }
+        return Content(JsonConvert.SerializeObject(obj), "application/json");
     }
 }
